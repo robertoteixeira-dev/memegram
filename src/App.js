6b928ef8e4802929e4c088f8aca1f5d2e61bc582
Roberto Teixeira <robertoteixeira.developer@gmail.com>
2022-03-05 16:19:57 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Header } from './components/Header';
+import './styles/global.css'
+
+function App() {
+  return (
+    <Header />
+    
+  );
+}
+
+export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,21 @@
+export function Header() {
+    return (
+        <header className="header">
+
+            <div className="container">
+
+                <img className="logo" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" />
+
+                <div className="input-fake">
+                    icone
+                    <input />
+                </div>
+
+                <div>
+                    icones
+                </div>
+
+            </div>
+        </header>
+    )
+}
