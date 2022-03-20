3b8a702cd16f1db2edc58024e6260cd0beb80d08
Roberto Teixeira <robertoteixeira.developer@gmail.com>
2022-03-19 23:41:40 -0300

    routes

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -4,7 +4,6 @@ import { Story } from '../Story'
 import { Post } from '../Post'
 import { Suggestion } from '../Suggestion'
 import { useEffect, useState } from 'react'
-import { SignUp } from '../SignUp'
 
 export function Layout() {
 
--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -1,28 +1,9 @@
 import './style.css'
-import { useForm } from "react-hook-form";
-import { useEffect, useState } from 'react'
 
-let counter = 0; 
-
-export function SignUp(){
-    
-    const onSubmit = (d) =>
-    alert(JSON.stringify(d));
+export function Signup() {
 
     return(
-        <form onSubmit={onSubmit}>
-            <label>
-                Username:
-                <input name="username" />
-            </label>
-
-            <label>
-                Email:
-                <input name="email" />
-            </label>
-
-            <p>Render: <span>{counter++}</span></p>
-            <input type="submit" value="submit" />
-        </form>
-    );
+        <>
+        </>
+    )
 }
